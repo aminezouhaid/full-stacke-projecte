15aaf7f5b981ee46d9f6b650f48b27fb12ae5445
Zakaria <elismailizakariae@gmail.com>
2021-04-19 11:21:14 +0000

    addinge header and footer for produit

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,3 +1,26 @@
+	<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+		
+		<title>Document</title>
+	</head>
+	<body>
+		
+	<footer class="bg-light text-center text-lg-start">
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+          © 2021 Copyright:
+          <a class="text-dark" href="index.html">contactlist.com</a>
+        </div>
+        <!-- Copyright -->
+      </footer>
+	
+	
+	
 	<script type="text/javascript" src="<?php echo URLROOT; ?>/js/main.js"></script>
 </body>
 </html>
--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -1,8 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/css/style.css">
 	<title><?php echo SITENAME; ?></title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-secondary navbar-dark">
+            <div class="container">
+                <button class="btn" disabled>
+                    <a class="navbar-brand spinner-grow spinner-grow-sm " href="#"></a>
+                  </button>
+                  <a class="navbar-brand " href="#">Contact List</a>
+              
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+                <form class="d-flex ">
+                  <a class="btn btn-outline-light bg-dark navbar-light text-light" type="button" href="index.html"> Login </a>
+                </form>
+              </div>
+          </nav>
+		  </body>
+		  </html>
 
